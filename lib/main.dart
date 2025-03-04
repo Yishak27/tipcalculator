e1989290c30b8c6c9c60c31d464e0fdfb6b2652d
PC <PC@DESKTOP-F726TKR>
2025-03-04 13:36:15 +0300

    designining the general skellaton

--- lib/main.dart
+++ lib/main.dart
@@ -30,6 +30,21 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   bool istoggeled = false;
   String amountByPerson = "0";
+  int _counter = 0;
+  double amount = 0.0;
+
+  void _increment() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  void _decrement() {
+    setState(() {
+      _counter--;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -56,8 +71,91 @@ class _MyHomePageState extends State<MyHomePage> {
           Card(
             color: Colors.blue,
             child: ListTile(
+              textColor: Colors.white,
               title: Center(child: const Text("Total per person")),
               subtitle: Center(child: Text(amountByPerson)),
+            ),
+          ),
+          Card(
+            color: Colors.white,
+            shadowColor: Colors.amber,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(10)),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(10.5),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  TextField(
+                    decoration: InputDecoration(
+                      labelText: "Enter Bill Amount",
+                      border: OutlineInputBorder(),
+                      prefixIcon: Icon(Icons.attach_money),
+                    ),
+                    onChanged: (value) {
+                      setState(() {
+                        amountByPerson = value;
+                      });
+                    },
+                  ),
+
+                  Divider(thickness: 0.4),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    spacing: 10,
+                    children: [
+                      Text(
+                        "Splint",
+                        style: (TextStyle(fontSize: 18, color: Colors.blue)),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        spacing: 1,
+                        children: [
+                          IconButton(
+                            icon: const Icon(Icons.remove, color: Colors.blue),
+                            onPressed: _decrement,
+                          ),
+                          Text(
+                            '$_counter',
+                            style: const TextStyle(
+                              fontSize: 24,
+                              color: Colors.blue,
+                            ),
+                          ),
+                          IconButton(
+                            icon: const Icon(Icons.add, color: Colors.blue),
+                            onPressed: _increment,
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  Divider(thickness: 0.4),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    spacing: 10,
+                    children: [
+                      Text(
+                        "Tips",
+                        style: TextStyle(fontSize: 16, color: Colors.blue),
+                      ),
+                      Text("20.20"),
+                    ],
+                  ),
+                  Slider(
+                    min: 0,
+                    max: 100,
+                    value: amount,
+                    onChanged: (double newValue) {
+                      setState(() {
+                        amount = newValue;
+                      });
+                    },
+                  ),
+                ],
+              ),
             ),
           ),
         ],
